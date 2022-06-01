fccb88359598fb142ddb14a52d5155b13d68034c
IvyZ23 <ivy.zhu23@sitechhs.com>
2022-05-31 20:41:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require.paths.push('/usr/local/lib/node_modules');
 const express = require("express"); //this file requires express
 const port = process.env.PORT || 5000; //use external server port OR local 5000
 const app = express(); //instantiate express
